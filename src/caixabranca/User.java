a0aab283cd6a9b781bb8657297fd95433ea51623
alopes-tenor <83618366+alopes-tenor@users.noreply.github.com>
2023-10-04 22:33:08 -0300

    Enumeração código

--- src/caixabranca/User.java
+++ src/caixabranca/User.java
@@ -1,4 +1,5 @@
 
+
 package caixabranca;
 
 import java.sql.Connection;
@@ -7,30 +8,30 @@ import java.sql.ResultSet;
 import java.sql.Statement;
 
 public class User {
-    public Connection conectarBD(){
+   5. public Connection conectarBD(){
         Connection conn = null;
-        try{
-            Class.forName("com.mysql.Driver.Manager").newInstance(); //ForName deve chamar a String ("com.mysql.jdbc.Driver") para carregar o driver;
-            String url = "jdbc:mysql://127.0.0.1/test?user=lopes&password=123";
-            conn = DriverManager.getConnection(url);
-        }catch (Exception e){}
-        return conn;}
-       public String nome="";
-       public boolean result = false;
-       public boolean verificarUsuario(String login, String senha){
+    6.    try{
+          6.  Class.forName("com.mysql.Driver.Manager").newInstance(); //ForName deve chamar a String ("com.mysql.jdbc.Driver") para carregar o driver;
+          6.  String url = "jdbc:mysql://127.0.0.1/test?user=lopes&password=123";
+          6.  conn = DriverManager.getConnection(url);
+      7.  }catch (Exception e){}
+      8.  return conn;}
+      1. public String nome="";
+      2. public boolean result = false;
+      3. public boolean verificarUsuario(String login, String senha){
          String sql = "";
-         Connection conn = conectarBD();
+        4. Connection conn = conectarBD();
          //Instrução SQL
          sql += "select nome from usuarios";
          sql += "where login = " + "'" + login + "'";
          sql += "and senha = " + "'" + senha + "',";
-         try{
-             Statement st = conn.createStatement();
-             ResultSet rs = st.executeQuery(sql);
-             if(rs.next()){
-                 result = true;
-                 nome = rs.getString("nome");}
-             }catch (Exception e){}
-         return result;}
+        9. try{
+          9.   Statement st = conn.createStatement();
+           9.  ResultSet rs = st.executeQuery(sql);
+            10. if(rs.next()){
+              10.   result = true;
+               10.  nome = rs.getString("nome");}
+            11. }catch (Exception e){}
+        12. return result;}
          }
 
